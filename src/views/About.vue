7a157ba6951429462334adf46567b01613ba67fb
rafajlpz <frilo_94@hotmail.com>
2022-02-08 09:27:52 +0100

    V. 76

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1>Mi contador: {{ $store.state.contador }}</h1>
+    <input type="text" v-model="texto">
+    <h2>{{texto}}</h2>
   </div>
 </template>
+
+<script>
+
+export default{
+  data() {
+    return {
+      texto: ''
+    }
+  },
+}
+</script>
+
